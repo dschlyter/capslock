f9b595390ba3423fbd71ae67ee44e8b34ae8391f
David Schlyter <dschlyter@gmail.com>
2016-01-03 17:36:45 +0100

    comment out backspace remapping, no longer needed and annoying for other people

--- AutoHotkey.ahk
+++ AutoHotkey.ahk
@@ -1,7 +1,7 @@
 #SingleInstance force
 
 ; Use caps lock for backspace, add a trap on backspace key for faster learning
-$Backspace::Send, WRONG
+; $Backspace::Send, WRONG
 $Capslock::Backspace
 $^Capslock::^Backspace
 +Capslock::Capslock
--- osx_setup.sh
+++ osx_setup.sh
@@ -6,8 +6,8 @@ cli=$app/Contents/Library/bin/seil
 
 $cli set enable_capslock 1
 $cli set keycode_capslock 51
-$cli set enable_delete 1
-$cli set keycode_delete 48
-echo 'mapped normal backspace to tab to facilitate learning, run "seil set enable_delete 0" to re-enable'
+# $cli set enable_delete 1
+# $cli set keycode_delete 48
+# echo 'mapped normal backspace to tab to facilitate learning, run "seil set enable_delete 0" to re-enable'
 
 open $app # this seems to be needed to apply the mappings
